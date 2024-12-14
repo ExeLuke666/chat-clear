352eaa73bd06c1b57cb6a3aeada623b02e3e34bb
ProgramLuke <85005142+ExeLuke666@users.noreply.github.com>
2024-12-13 23:34:53 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,9 @@
--- Resource Metadata
 fx_version 'cerulean'
 games { 'rdr3', 'gta5' }
 
-author 'NAT2K15'
-description 'FiveM clear chat'
+author 'Luke Development'
+description 'FiveM Clear Chat Script - For support, visit https://lukedevelopment.xyz'
 
 server_scripts {
   'server.lua'
-} 
+}
